713b96fca2dace271e807d07f36e7834d0b9b5a6
lauracastrovenegas <61126997+lauracastrovenegas@users.noreply.github.com>
2022-10-15 17:36:21 -0400

    Create Projects.js

---
+++ src/containers/Projects.js
@@ -0,0 +1,12 @@
+import AllProjects from '../components/AllProjects';
+import SubPageWrapper from './SubPageWrapper';
+
+const Projects = () => {
+  return (
+    <SubPageWrapper title="Projects Portfolio">
+        <AllProjects />
+    </SubPageWrapper>
+  );
+}
+
+export default Projects;
