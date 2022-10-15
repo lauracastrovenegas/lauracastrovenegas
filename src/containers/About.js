75f3502dffdd6296047e106900fd73595bd06c02
lauracastrovenegas <61126997+lauracastrovenegas@users.noreply.github.com>
2022-10-15 17:36:13 -0400

    Create About.js

---
+++ src/containers/About.js
@@ -0,0 +1,11 @@
+import SubPageWrapper from './SubPageWrapper';
+
+const About = () => {
+  return (
+    <SubPageWrapper title="Laura Castro Venegas">
+        
+    </SubPageWrapper>
+  );
+}
+
+export default About;
