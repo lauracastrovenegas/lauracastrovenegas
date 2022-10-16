a533db36777a7fc39472985c291c41db5feabd0c
lauracastrovenegas <61126997+lauracastrovenegas@users.noreply.github.com>
2022-10-15 23:06:13 -0400

    Update Resume.js

--- src/containers/Resume.js
+++ src/containers/Resume.js
@@ -1,8 +1,7 @@
 import styled from 'styled-components/macro';
-import LCVResume from '../assets/Laura-Castro-Venegas-Resume-2022.pdf'
 import SubPageWrapper from './SubPageWrapper';
-import ButtonWrapper from '../components/ButtonWrapper';
 import theme from '../theme';
+import DownloadResumeButton from '../components/DownloadResumeButton';
 
 const EmbeddedDoc = styled.div`
   display: flex;
@@ -18,28 +17,13 @@ const EmbeddedDoc = styled.div`
   }
 `
 
-const Button = styled.div`
-    max-width: 15rem;
-    margin: 0rem auto;
-`
-
-const Label = styled.div`
-    margin: auto 1rem;
-    color: white;
-`
-
-const Content = styled.div`
-    margin: 0rem auto;
-    display: flex;
-`
-
 const Resume = () => {
   return (
     <SubPageWrapper title="">
       <EmbeddedDoc>
         <embed src="https://docs.google.com/document/d/e/2PACX-1vSAbn3yih-qKpedVZaSgIbbbKim1qdRAvBdC2KkOw94crAE_kkHDkXJfhMXd1ykzo6pmT-MW7D0TlGr/pub?embedded=true"></embed>
       </EmbeddedDoc>
-      <DownloadButton/>
+      <DownloadResumeButton/>
         {/* <iframe src={"https://docs.google.com/document/d/e/2PACX-1vSAbn3yih-qKpedVZaSgIbbbKim1qdRAvBdC2KkOw94crAE_kkHDkXJfhMXd1ykzo6pmT-MW7D0TlGr/pub"} style={{width: '100%', height: '90vh'}}></iframe> */}
     </SubPageWrapper>
   );
@@ -47,14 +31,3 @@ const Resume = () => {
 
 export default Resume;
 
-const DownloadButton = () => {
-  return (
-    <Button>
-      <ButtonWrapper link="https://docs.google.com/document/d/1lmRXsrpDICmdPVsx14Al5RSfuBFstQvVy_imo21Er20/export?format=pdf">
-          <Content>
-              <Label>Download Resume</Label>
-          </Content>
-      </ButtonWrapper>
-     </Button>
-  )
-}
