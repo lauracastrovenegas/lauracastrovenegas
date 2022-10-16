4da57d6de851b24eb1dc62e61e28952d493c327e
lauracastrovenegas <61126997+lauracastrovenegas@users.noreply.github.com>
2022-10-15 23:06:32 -0400

    Added profile image to about me page

--- src/containers/About.js
+++ src/containers/About.js
@@ -1,9 +1,14 @@
 import SubPageWrapper from './SubPageWrapper';
 import styled from 'styled-components';
 import theme from '../theme';
+import SocialMediaLinks from '../components/SocialMediaLinks';
+import ResumeButton from '../components/ResumeButton';
+import ProjectsButton from '../components/ProjectsButton';
+import DownloadResumeButton from '../components/DownloadResumeButton';
+import laura from '../images/laura.jpg'
 
 const Content = styled.div`
-  padding: 5rem;
+  padding: 2rem 2rem;
   color: white;
 
   @media only screen and (max-width: ${theme.sizes.tablet}){
@@ -11,25 +16,85 @@ const Content = styled.div`
   }
 `
 
-const SubTitle = styled.div`
-  color: white;
-  font-size: 3rem;
+const MobileOnly = styled.div`
+  display: none;
+
+  @media only screen and (max-width: ${theme.sizes.tablet}){
+    display: flex;
+    flex-direction: column;
+    width: fit-content;
+    margin: 0rem auto;
+  }
+`
+
+const Media = styled.div`
+  width: 15rem;
+  margin: 0rem auto;
+`
+
+const DesktopOnly = styled.div`
+  display: flex;
+  width: fit-content;
   margin: 0rem auto;
 
   @media only screen and (max-width: ${theme.sizes.tablet}){
-    font-size: 20px;
+    display: none;
+  }
+`
+
+const Top = styled.div`
+  display: flex;
+  width: fit-content;
+  margin: 1rem 1rem;
+
+  @media only screen and (max-width: ${theme.sizes.tablet}){
+    flex-direction: column;
+  }
+`
+
+const Button = styled.div`
+  margin: 0rem 1rem;
+  width: fit-content;
+`
+const ProfilePic = styled.div`
+  max-width: 20rem;
+  margin: auto auto;
+
+  img {
+    width: 100%;
+    object-fit: contain;
   }
 `
 
 const About = () => {
   return (
     <SubPageWrapper title="About Me">
-        <SubTitle>Laura Castro Venegas</SubTitle>
+      <Top>
+        <ProfilePic>
+          <img src={laura}/>
+        </ProfilePic>
         <Content>
-          <p>Hello! I am a fourth-year student at Columbia University in the School of Engineering and Applied Sciences majoring in Computer Science on the Applications Track with a minor in Applied Mathematics.</p>
-          <p>My interests lie in User-Centered Design and Human-Computer Interaction. I have extensive esperience in Full-Stack development and spend most of my time working on React and React Native Projects.</p>
-          <p>I am currently working at the Columbia Daily Spectator Newspaper as Head of Engineering and Full-Stack JavaScript Developer.</p>
+          <p>Hello, I'm <b>Laura Castro Venegas</b>!</p>
+          <p>I am a fourth-year student at Columbia University in the School of Engineering and Applied Sciences, majoring in Computer Science on the Applications Track and minoring in Applied Mathematics.</p>
+          <p>My interests lie in User-Centered Design, Human-Computer Interaction, and Frontend Development. I have extensive experience in Full-Stack Javascript development and spend most of my time working on React and React Native Projects.</p>
+          <p>I am the Head of Engineering at the Columbia Spectator, where I lead the Engineering Team and work as a Full-Stack JavaScript Developer.</p>
         </Content>
+      </Top>
+      <DesktopOnly>
+        <Button>
+          <ProjectsButton className="button" />
+        </Button>
+        <Button>
+          <ResumeButton className="button" />
+        </Button>
+      </DesktopOnly>
+      <MobileOnly>
+        <ProjectsButton />
+        <DownloadResumeButton />
+        <Media>
+          <SocialMediaLinks />
+        </Media>
+      </MobileOnly>
     </SubPageWrapper>
   );
 }
